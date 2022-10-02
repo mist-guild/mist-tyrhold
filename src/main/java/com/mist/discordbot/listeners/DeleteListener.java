0b42386b6ad11f1154854b581a8594dbe956d6b4
Zak Pruitt <pruittzn@gmail.com>
2022-10-02 02:56:41 -0500

    Implemented DeleteListener

--- src/main/java/com/mist/discordbot/listeners/DeleteListener.java
+++ src/main/java/com/mist/discordbot/listeners/DeleteListener.java
@@ -1,4 +1,17 @@
 package com.mist.discordbot.listeners;
 
-public class DeleteListener {
+import org.javacord.api.event.message.reaction.ReactionAddEvent;
+import org.javacord.api.listener.message.reaction.ReactionAddListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteListener implements ReactionAddListener {
+
+
+    @Override
+    public void onReactionAdd(ReactionAddEvent reactionAddEvent) {
+        if(reactionAddEvent.getEmoji().equalsEmoji("\uD83D\uDC4E")) {
+            reactionAddEvent.deleteMessage();
+        }
+    }
 }
