51dd16e6c8f29588b60aedcd3cec6f9da9148303
Zak Pruitt <pruittzn@gmail.com>
2022-10-02 23:40:33 -0500

    Added in regex to check channel name

--- src/main/java/com/mist/discordbot/listeners/DeleteRecruitChannelListener.java
+++ src/main/java/com/mist/discordbot/listeners/DeleteRecruitChannelListener.java
@@ -1,12 +1,23 @@
 package com.mist.discordbot.listeners;
 
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DeleteRecruitChannelListener implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
+        String message = messageCreateEvent.getMessageContent();
+        ServerTextChannel channel = (ServerTextChannel) messageCreateEvent.getChannel();
 
+        if (message.equals("!endtrial")) {
+            if (channel.getName().matches("\\d+-trial-.+")) {
+
+            }
+        }
     }
 }
