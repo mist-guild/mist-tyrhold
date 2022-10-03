bf25d84d4493c1c270a328d06b62b4a9d3c49db0
Zak Pruitt <pruittzn@gmail.com>
2022-10-02 23:46:18 -0500

    Added in channel delete functionality

--- src/main/java/com/mist/discordbot/listeners/DeleteRecruitChannelListener.java
+++ src/main/java/com/mist/discordbot/listeners/DeleteRecruitChannelListener.java
@@ -1,13 +1,18 @@
 package com.mist.discordbot.listeners;
 
+import com.mist.discordbot.Services.MessagingService;
 import org.javacord.api.entity.channel.Channel;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DeleteRecruitChannelListener implements MessageCreateListener {
+
+    @Autowired
+    private MessagingService messagingService;
 
     @Override
     public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
@@ -16,8 +21,17 @@ public class DeleteRecruitChannelListener implements MessageCreateListener {
 
         if (message.equals("!endtrial")) {
             if (channel.getName().matches("\\d+-trial-.+")) {
-
+                channel.delete();
             }
+            messagingService.sendMessage(
+                    null,
+                    null,
+                    "Please ensure you execute !endtrial in a trial channel! It should follow this naming pattern: id-trial-name.",
+                    "This message will delete itself in 1 minute. 💣",
+                    null,
+                    messageCreateEvent.getChannel(),
+                    true
+            );
         }
     }
 }
