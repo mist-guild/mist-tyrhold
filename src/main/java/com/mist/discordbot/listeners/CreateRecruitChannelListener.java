7e288b771aa97e42a5e99360cc95444a4b5eaf88
Zak Pruitt <pruittzn@gmail.com>
2022-10-02 21:27:40 -0500

    Added initial implementation and checks to trial

--- src/main/java/com/mist/discordbot/listeners/CreateRecruitChannelListener.java
+++ src/main/java/com/mist/discordbot/listeners/CreateRecruitChannelListener.java
@@ -1,10 +1,14 @@
 package com.mist.discordbot.listeners;
 
 import com.mist.discordbot.Services.MessagingService;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Component
 public class CreateRecruitChannelListener implements MessageCreateListener {
@@ -14,8 +18,18 @@ public class CreateRecruitChannelListener implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
-        System.out.println(messageCreateEvent.getChannel());
-        if (false) {
+        Long channelId = messageCreateEvent.getChannel().getId();
+        String message = messageCreateEvent.getMessageContent();
+
+        if (channelId.equals(1026048915688140800L) && message.startsWith("!trial")) {
+            String[] split = message.split(" ");
+            if (split.length != 2) return;
+
+
+            System.out.println(split[0]);
+            System.out.println(split[1]);
+            System.out.println(split.length);
+
 
         }
     }
