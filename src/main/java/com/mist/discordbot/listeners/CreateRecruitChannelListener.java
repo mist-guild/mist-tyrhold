e10f7076fbc51a6149f4d1c61f79492d8e9658fc
Zak Pruitt <pruittzn@gmail.com>
2022-10-02 22:56:31 -0500

    added in code to generate category

--- src/main/java/com/mist/discordbot/listeners/CreateRecruitChannelListener.java
+++ src/main/java/com/mist/discordbot/listeners/CreateRecruitChannelListener.java
@@ -1,6 +1,7 @@
 package com.mist.discordbot.listeners;
 
 import com.mist.discordbot.Services.MessagingService;
+import org.javacord.api.entity.channel.*;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
@@ -23,14 +24,22 @@ public class CreateRecruitChannelListener implements MessageCreateListener {
 
         if (channelId.equals(1026048915688140800L) && message.startsWith("!trial")) {
             String[] split = message.split(" ");
-            if (split.length != 2) return;
+            if (split.length != 3) return;
 
+            String id = split[1];
+            String name = split[2];
 
-            System.out.println(split[0]);
-            System.out.println(split[1]);
-            System.out.println(split.length);
-
-
+            Optional<ChannelCategory> category = messageCreateEvent.getServer()
+                    .get()
+                    .getChannelCategoryById("992720873528246294");
+            if (category.isPresent()) {
+                messageCreateEvent.getServer()
+                        .get()
+                        .createTextChannelBuilder()
+                        .setName(String.format("%s-trial-%s", id, name))
+                        .setCategory(category.get())
+                        .create();
+            }
         }
     }
 }
