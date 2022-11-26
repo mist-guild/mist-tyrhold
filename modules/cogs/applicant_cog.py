0de6ab39565ef14a00d96178eb6550d10a3bc22e
Zak Pruitt <pruittzn@gmail.com>
2022-11-25 21:53:27 -0600

    converted application_service into a class

--- modules/cogs/applicant_cog.py
+++ modules/cogs/applicant_cog.py
@@ -1,7 +1,7 @@
 import os
 import re
 from discord.ext import commands
-from modules.services import applicant_service
+from modules.services.applicant_service import ApplicantService
 from modules.dataclasses.applicant import Applicant
 from modules.utility.discord_utility import DiscordUtility
 
@@ -20,7 +20,7 @@ class ApplicantCog(commands.Cog, name="Applicant"):
         # post applicant embed
         id = DiscordUtility.get_applicant_id_from_recruit_channel_name(
             channel.name)
-        embed, applicant = applicant_service.get_applicant_and_embed(id)
+        embed, applicant = ApplicantService.get_applicant_and_embed(id)
         message = await channel.send(embed=embed)
 
         # pin and add reactions
@@ -30,7 +30,7 @@ class ApplicantCog(commands.Cog, name="Applicant"):
             await message.add_reaction(emoji)
 
         # check if applicant is already in database
-        previous_apps = applicant_service.get_previous_applications(applicant)
+        previous_apps = ApplicantService.get_previous_applications(applicant)
         for id in previous_apps:
             await channel.send(f"**Previous Application detected!** Run `!mb app {id}` to view it.")
 
@@ -40,7 +40,7 @@ class ApplicantCog(commands.Cog, name="Applicant"):
         if re.match('.{2}-.+-\d+', ctx.channel.name):
             id = DiscordUtility.get_applicant_id_from_recruit_channel_name(
                 ctx.channel.name)
-            await applicant_service.archive_comments(id, ctx.channel)
+            await ApplicantService.archive_comments(id, ctx.channel)
             await ctx.channel.delete()
 
     @commands.command("app")
@@ -50,14 +50,14 @@ class ApplicantCog(commands.Cog, name="Applicant"):
             return
 
         # create archive channel
-        new_channel = await applicant_service.generate_archive_text_channel(id, ctx.guild)
+        new_channel = await ApplicantService.generate_archive_text_channel(id, ctx.guild)
 
         # get applicant and post old app
-        embed, applicant = applicant_service.get_applicant_and_embed(id)
+        embed, applicant = ApplicantService.get_applicant_and_embed(id)
         await new_channel.send(embed=embed)
 
         # post archived messages
-        applicant_service.post_archived_messages(applicant, new_channel)
+        ApplicantService.post_archived_messages(applicant, new_channel)
 
 
 async def setup(bot: commands.Bot):
